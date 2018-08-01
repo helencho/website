a40d6564085c844a1788d9bcee489ac61390e8c3
helencho <helencho@ac.c4q.nyc>
2018-08-01 11:34:45 -0400

    add hover over animation

--- src/components/elements/ProjectImage.js
+++ src/components/elements/ProjectImage.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
 class ProjectImage extends Component {
+    state = {
+        hover: false 
+    }
+
+    toggleHover = () => {
+        this.setState({
+            hover: !this.state.hover
+        })
+    }
+
     render() {
+        console.log(this.state) 
+
         return (
-            <img src={this.props.url} alt="Image text" />
+            <div onMouseOver={this.toggleHover} onMouseLeave={this.toggleHover} className="ProjectImage">
+                <img src={this.props.url} alt="Image text" />
+                <div className="overlay">
+                    <div className="text">
+                        <h3>Elevate</h3>
+                        <p>Project description goes here</p>
+                        <p>Postgres, Express, React, Node.js</p>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
