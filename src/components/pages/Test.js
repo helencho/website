7d23ff48e950e55db028917c0cbbab772940dc0b
helencho <helencho@ac.c4q.nyc>
2018-08-01 11:46:37 -0400

    wrap project images in a section

--- src/components/pages/Test.js
+++ src/components/pages/Test.js
@@ -10,11 +10,38 @@ class Test extends Component {
                 <h1>Test</h1>
                 <Button text="Primary" />
                 <p>I walked 5 miles in <Anchor text="Bear Mountain" /> yesterday.</p>
-                <ProjectImage 
-                    url="./assets/projects/peter-screely.png"
-                    name="Sh*t Peter Says"
-                    description="Farewell gift to the instructor"
-                    stacks="JavaScript, CSS" />
+                <section className="showcase">
+                    <ProjectImage 
+                        url="./assets/projects/peter-screely.png"
+                        name="Sh*t Peter Says"
+                        description="Farewell gift to the instructor"
+                        stacks="JavaScript, CSS" />
+                    <ProjectImage 
+                        url="./assets/projects/peter-screely.png"
+                        name="Sh*t Peter Says"
+                        description="Farewell gift to the instructor"
+                        stacks="JavaScript, CSS" />
+                    <ProjectImage 
+                        url="./assets/projects/peter-screely.png"
+                        name="Sh*t Peter Says"
+                        description="Farewell gift to the instructor"
+                        stacks="JavaScript, CSS" />
+                    <ProjectImage 
+                        url="./assets/projects/peter-screely.png"
+                        name="Sh*t Peter Says"
+                        description="Farewell gift to the instructor"
+                        stacks="JavaScript, CSS" />
+                    <ProjectImage 
+                        url="./assets/projects/peter-screely.png"
+                        name="Sh*t Peter Says"
+                        description="Farewell gift to the instructor"
+                        stacks="JavaScript, CSS" />
+                    <ProjectImage 
+                        url="./assets/projects/peter-screely.png"
+                        name="Sh*t Peter Says"
+                        description="Farewell gift to the instructor"
+                        stacks="JavaScript, CSS" />
+                </section>
             </div>
         )
     }
