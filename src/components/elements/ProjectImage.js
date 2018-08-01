e18436762d86144861044f1d278b034b5ac6cd76
helencho <helencho@ac.c4q.nyc>
2018-08-01 11:40:21 -0400

    finetune styles and delete unused functions

--- src/components/elements/ProjectImage.js
+++ src/components/elements/ProjectImage.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
 
 class ProjectImage extends Component {
-    state = {
-        hover: false 
-    }
-
-    toggleHover = () => {
-        this.setState({
-            hover: !this.state.hover
-        })
-    }
-
     render() {
-        console.log(this.state) 
-
         return (
-            <div onMouseOver={this.toggleHover} onMouseLeave={this.toggleHover} className="ProjectImage">
+            <div className="ProjectImage">
                 <img src={this.props.url} alt="Image text" />
                 <div className="overlay">
                     <div className="text">
-                        <h3>Elevate</h3>
-                        <p>Project description goes here</p>
-                        <p>Postgres, Express, React, Node.js</p>
+                        <h3>{this.props.name}</h3>
+                        <p>{this.props.description}</p>
+                        <p>{this.props.stacks}</p>
                     </div>
                 </div>
             </div>
--- src/components/pages/Test.js
+++ src/components/pages/Test.js
@@ -10,7 +10,11 @@ class Test extends Component {
                 <h1>Test</h1>
                 <Button text="Primary" />
                 <p>I walked 5 miles in <Anchor text="Bear Mountain" /> yesterday.</p>
-                <ProjectImage url="./assets/projects/peter-screely.png" />
+                <ProjectImage 
+                    url="./assets/projects/peter-screely.png"
+                    name="Sh*t Peter Says"
+                    description="Farewell gift to the instructor"
+                    stacks="JavaScript, CSS" />
             </div>
         )
     }
