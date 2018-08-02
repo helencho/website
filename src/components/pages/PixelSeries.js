471e5febdbf212e13f3e63e6d8e63eaf670c1dec
helencho <helencho@ac.c4q.nyc>
2018-08-02 14:23:01 -0400

    pull pixel data and render on component

--- src/components/pages/PixelSeries.js
+++ src/components/pages/PixelSeries.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Pixel from '../elements/Pixel';
+import pixelData from '../../db/pixel.json';
 
 class PixelSeries extends Component {
     render() {
         return (
-            <div className="PixelSeries">
-                <h1>PixelSeries</h1>
+            <div className="PixelSeries fadein main-header">
+                <h1>Pixel Series</h1>
+                <div className="showcase">
+                    {pixelData.map((pixel, id) => <Pixel data={pixel} key={id} />)}
+                </div>
             </div>
         )
     }
