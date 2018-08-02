edcab4f633403523960bf89fa1f3a888896fd16a
helencho <helencho@ac.c4q.nyc>
2018-08-02 14:13:11 -0400

    style navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,12 @@ class NavBar extends Component {
     render() {
         return (
             <nav className="NavBar">
-                <Link to="/" className="Link">H</Link>
-                <Link to="/projects" className="Link">Projects</Link>
-                <Link to="/pixel" className="Link">Pixel</Link>
-                <Link to="/about" className="Link">About</Link>
+                <Link to="/" className="Link"><img src="./assets/icons/logo-1.png" alt="H" className="logo" /></Link>
+                <div className="nav-links">
+                    <Link to="/projects" className="Link">Projects</Link>
+                    <Link to="/pixel" className="Link">Pixel</Link>
+                    <Link to="/about" className="Link">About</Link>
+                </div>
             </nav>
         )
     }
