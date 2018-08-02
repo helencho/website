f6319ec89be535eaa1a81003e013c01134d0ef52
helencho <helencho@ac.c4q.nyc>
2018-08-02 17:06:07 -0400

    add minor details about myself on homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,17 @@ class Home extends Component {
     render() {
         return (
             <div className="Home fadein">
-                <h1 className="main-header">Hi, my name is Helen.</h1>
+                <h1 className="main-header">Hi, my name is Helen</h1>
+                <main>
+                    <p>runner</p>
+                    <p>writer</p>
+                    <p>reader</p>
+                    <p>designer</p>
+                    <p>doodler</p>
+                    <p>hiker</p>
+                    <p>tea drinker</p>
+                    <p>software engineer.</p>
+                </main>
             </div>
         )
     }
