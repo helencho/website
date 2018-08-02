f22be3d8747020630f6032ac3b2683bf0f902925
helencho <helencho@ac.c4q.nyc>
2018-08-02 14:20:45 -0400

    pull project data and render data images under component

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import ProjectImage from '../elements/ProjectImage';
+import projectsData from '../../db/projects.json';
 
 class Projects extends Component {
     render() {
         return (
-            <div className="Projects">
+            <div className="Projects fadein main-header">
                 <h1>Projects</h1>
+                <section className="showcase">
+                    {projectsData.map((project, id) => <ProjectImage data={project} key={id} />)}
+                </section>
             </div>
         )
     }
