f593d4b2c65333794947edb51cad449a33235cb3
helencho <helencho@ac.c4q.nyc>
2018-08-03 18:04:29 -0400

    ensure the number of images matches with overlay components and titles

--- src/tests/projects.test.js
+++ src/tests/projects.test.js
@@ -3,7 +3,7 @@ import Projects from '../components/pages/Projects';
 import ProjectImage from '../components/elements/ProjectImage';
 import ProjectModal from '../components/elements/ProjectModal';
 import projectData from '../db/projects.json';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 it('renders without crashing', () => {
     expect(shallow(<Projects />).find('.Projects').exists()).toBe(true);
@@ -59,18 +59,19 @@ describe('projects data object contains', () => {
     });
 });
 
-// describe('there are matching', () => {
-//     it('images and titles', () => {
-//         const images = shallow(<Projects />).find('img').length;
-//         const components = shallow(<Projects />).find('h3').length;
-//         expect(images).toEqual(components);
-//     });
-//     it('images and overlay components', () => {
-//         const images = shallow(<Projects />).find(<img />).length;
-//         const overlay = shallow(<Projects />).find('.overlay').length;
-//         expect(images).toEqual(overlay);
-//     });
-// });
+describe('there are matching', () => {
+    it('images to hidden overlay components', () => {
+        const images = mount(<Projects />).find('img').length;
+        const overlay = mount(<Projects />).find('.overlay').length;
+        expect(images).toEqual(overlay);
+    });
+
+    it('images to project titles', () => {
+        const images = mount(<Projects />).find('img').length;
+        const components = mount(<Projects />).find('h3').length;
+        expect(images).toEqual(components);
+    });
+});
 
 describe('<ProjectModal />', () => {
     it('open modal when .overlay is clicked', () => {
