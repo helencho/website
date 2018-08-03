e789f61fa60f2d2186481970e75cb6370a3ae4f4
helencho <helencho@ac.c4q.nyc>
2018-08-03 17:50:15 -0400

    fix test from shallow to mount

--- src/tests/pixelseries.test.js
+++ src/tests/pixelseries.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PixelSeries from '../components/pages/PixelSeries';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 it('renders without crashing', () => {
     expect(shallow(<PixelSeries />).find('.PixelSeries').exists()).toBe(true);
 });
 
 it('renders same numbers of images and names', () => {
-    const images = shallow(<PixelSeries/>).find('img').length;
-    const names = shallow(<PixelSeries/>).find('p').length;
+    const images = mount(<PixelSeries/>).find('img').length;
+    const names = mount(<PixelSeries/>).find('p').length;
     expect(images).toEqual(names);
 });
