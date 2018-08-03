3b314e8028fc2339431f551b5b8a68df63a8d72c
helencho <helencho@ac.c4q.nyc>
2018-08-03 18:01:39 -0400

    ensure key-value pairs exist

--- src/tests/pixelseries.test.js
+++ src/tests/pixelseries.test.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import PixelSeries from '../components/pages/PixelSeries';
+import pixelData from '../db/pixel.json';
 import { shallow, mount } from 'enzyme';
 
 it('renders without crashing', () => {
     expect(shallow(<PixelSeries />).find('.PixelSeries').exists()).toBe(true);
 });
 
-it('renders same numbers of images and names', () => {
-    const images = mount(<PixelSeries/>).find('img').length;
-    const names = mount(<PixelSeries/>).find('p').length;
-    expect(images).toEqual(names);
+describe('pixel data object contains', () => {
+    it('name', () => {
+        pixelData.map(pixel => {
+            expect(pixel.name).toBeTruthy();
+        });
+    });
+
+    it('image url', () => {
+        pixelData.map(pixel => {
+            expect(pixel.image).toBeTruthy();
+        });
+    });
+});
+
+describe('there are matching', () => {
+    it('images to names', () => {
+        const images = mount(<PixelSeries/>).find('img').length;
+        const names = mount(<PixelSeries/>).find('p').length;
+        expect(images).toEqual(names);
+    });
 });
