544a29da7aa3befa5b52f0725409104496da947a
helencho <helencho@ac.c4q.nyc>
2018-08-07 12:47:33 -0400

    add homepage welcome/intro and style font for each media stop point

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,16 +4,10 @@ class Home extends Component {
     render() {
         return (
             <div className="Home fadein">
-                <h1 className="main-header">Hi, my name is Helen</h1>
                 <main>
-                    <p>runner</p>
-                    <p>writer</p>
-                    <p>reader</p>
-                    <p>designer</p>
-                    <p>doodler</p>
-                    <p>hiker</p>
-                    <p>tea drinker</p>
-                    <p>software engineer.</p>
+                    <h1 className="fadein-1">Hi.</h1>
+                    <h1 className="fadein-2">My name is Helen.</h1>
+                    <h1 className="fadein-3">I'm a software engineer.</h1>
                 </main>
             </div>
         )
